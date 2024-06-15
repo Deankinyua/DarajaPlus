9b0b3a2d97c2a552ff782791d2c014cfe0445526
deankinyua <kinyuadean@gmail.com>
2024-06-15 12:03:50 +0300

    looksss decent

--- src/services/EmailService.tsx
+++ src/services/EmailService.tsx
@@ -8,29 +8,29 @@ import {
 
 const EmailService = () => {
   return (
-    <FormControl size={"xl"}>
-      <FormLabel size={"xl"} fontSize={"1.5rem"}>
+    <FormControl size={"xl"} isRequired>
+      <FormLabel size={"xl"} fontSize={"1.3rem"}>
         Email Address
       </FormLabel>
-      <Input type="email" size={"25"} />
+      <Input type="email" size={"30"} />
 
       <FormErrorMessage>Email is required.</FormErrorMessage>
 
-      <FormLabel fontSize={"1.5rem"}>Name</FormLabel>
+      <FormLabel fontSize={"1.3rem"}>Name</FormLabel>
       <Input type="text" size={"20"} />
       <FormErrorMessage>Name is required.</FormErrorMessage>
 
-      <FormLabel fontSize={"1.5rem"}>Subject</FormLabel>
-      <Input type="text" height={"30"} />
+      <FormLabel fontSize={"1.3rem"}>Subject</FormLabel>
+      <Input type="text" height={"30"} size={"20"} />
 
-      <FormLabel fontSize={"1.5rem"}>Country</FormLabel>
-      <Select placeholder="Select country">
+      <FormLabel fontSize={"1.3rem"}>Country</FormLabel>
+      <Select placeholder="Select country" height={"30"}>
         <option>Kenya</option>
         <option>Rwanda</option>
         <option>Zambia</option>
       </Select>
 
-      <FormLabel size={"xl"} fontSize={"1.5rem"}>
+      <FormLabel size={"xl"} fontSize={"1.3rem"}>
         How can we help you? Feel free to get in touch
       </FormLabel>
       <Input type="text" size={"2xl"} height={"100"} />
