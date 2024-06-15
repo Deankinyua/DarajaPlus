0746dfd2476edcd224b0719d6d5f2433c48ec82d
deankinyua <kinyuadean@gmail.com>
2024-06-15 11:28:23 +0300

    jfdjjfjfjf

--- src/Components/ExtendTheme.tsx
+++
@@ -1,6 +0,0 @@
-import { extendTheme } from "@chakra-ui/react";
-import { modalTheme } from "./theme/modal";
-
-export const theme = extendTheme({
-  components: { Modal: modalTheme },
-});
--- src/services/EmailService.tsx
+++ src/services/EmailService.tsx
@@ -8,18 +8,18 @@ import {
 
 const EmailService = () => {
   return (
-    <FormControl>
-      <FormLabel>Email Address</FormLabel>
-      <Input type="email" placeholder="Enter your email" />
+    <FormControl size={"xl"}>
+      <FormLabel size={"xl"}>Email Address</FormLabel>
+      <Input type="email" size={"25"} />
 
       <FormErrorMessage>Email is required.</FormErrorMessage>
 
       <FormLabel>Name</FormLabel>
-      <Input type="text" placeholder="Name" />
+      <Input type="text" size={"20"} />
       <FormErrorMessage>Name is required.</FormErrorMessage>
 
       <FormLabel>Subject</FormLabel>
-      <Input type="text" placeholder="Name" />
+      <Input type="text" height={"30"} />
 
       <FormLabel>Country</FormLabel>
       <Select placeholder="Select country">
@@ -28,8 +28,10 @@ const EmailService = () => {
         <option>Zambia</option>
       </Select>
 
-      <FormLabel>How can we help you? Feel free to get in touch</FormLabel>
-      <Input type="text" placeholder="Name" />
+      <FormLabel size={"xl"} fontSize={"1.5rem"}>
+        How can we help you? Feel free to get in touch
+      </FormLabel>
+      <Input type="text" size={"2xl"} height={"100"} />
     </FormControl>
   );
 };
