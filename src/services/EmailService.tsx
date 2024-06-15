5abab28881d1df452b5ea22b41539016bf9fa603
deankinyua <kinyuadean@gmail.com>
2024-06-15 06:09:05 +0300

    hdjffkd

--- src/services/EmailService.tsx
+++ src/services/EmailService.tsx
@@ -1,30 +1,18 @@
 import {
   FormControl,
   FormLabel,
-  FormHelperText,
   Input,
   FormErrorMessage,
   Select,
 } from "@chakra-ui/react";
-import { useState } from "react";
 
 const EmailService = () => {
-  const [input, setInput] = useState("");
-
-  const handleInputChange = (e: MouseEvent) => setInput(e.target.value);
-
-  const isError = input === "";
   return (
     <FormControl>
       <FormLabel>Email address</FormLabel>
       <Input type="email" placeholder="Enter your email" />
-      {!isError ? (
-        <FormHelperText>
-          Enter the email you'd like to receive the newsletter on.
-        </FormHelperText>
-      ) : (
-        <FormErrorMessage>Email is required.</FormErrorMessage>
-      )}
+
+      <FormErrorMessage>Email is required.</FormErrorMessage>
 
       <FormLabel>Name</FormLabel>
       <Input type="text" placeholder="Name" />
