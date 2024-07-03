ce243491b7cf5cf719e25fb299cd92711496df36
Dean Kinyua <kinyuadean@gmail.com>
2024-07-03 08:55:06 +0300

    added an imaginary server component so as not to forget them

---
+++ src/Components/ServerComponentExample.tsx
@@ -0,0 +1,20 @@
+// * This is an Example of a react server component
+
+// import db from "imaginary-db";
+
+// async function Homepage() {
+//   const link = db.connect("localhost", "root", "passw0rd");
+//   const data = await db.query(link, "SELECT * FROM products");
+//   return (
+//     <>
+//       <h1>Trending Products</h1>
+//       {data.map((item) => (
+//         <article key={item.id}>
+//           <h2>{item.title}</h2>
+//           <p>{item.description}</p>
+//         </article>
+//       ))}
+//     </>
+//   );
+// }
+// export default Homepage;
